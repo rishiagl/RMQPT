145585bbe8fbe5ad16e1a1130bfdbeb4afc6e3ec
rishiagl <rishi58235@gmail.com>
2024-06-06 10:42:33 +0530

    final changes project in archived state

---
+++ presentation/rf_train.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pickle
+
+dataset_path = "Data_Set_2.csv"
+data = pd.read_csv(dataset_path, header=None)
+
+X = data.iloc[1:, 1:12]
+y = data.iloc[1:, 13]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(y_pred)
+print("Accuracy:", accuracy)
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
+
+with open('random_forest_model.pkl', 'wb') as f:
+    pickle.dump(clf, f)
